1b223ef88a2970647147b54210fc04532fa3d37b
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-04-17 00:22:53 +0200

    chore: update dependencies

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,25 +1,25 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use sp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{ProverClient, SP1Stdin};
 use trivial_tree;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    let serialized = trivial_tree::random_tree(4, 4);
+    let serialized = trivial_tree::random_tree(10, 10);
 
     // Generate proof.
     let mut stdin = SP1Stdin::new();
     stdin.write_slice(&serialized);
-    let proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(ELF, stdin).expect("proving failed");
 
     // Read output.
     let found = &proof.public_values.buffer.data;
-    // println!("expected:     {:x?}", db.hash());
     println!("found:        {:x?}", found);
 
     // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    // client.verify(ELF, &proof).expect("verification failed");
 
     // Save proof.
     proof
