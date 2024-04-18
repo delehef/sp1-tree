f42d4165b23f47f46080bb9b966c8fc129858377
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-04-18 16:24:11 +0200

    homogeneize tests

--- program/src/main.rs
+++ program/src/main.rs
@@ -7,7 +7,7 @@ pub fn main() {
     println!("parsing the tree...");
     let serialized_tree = sp1_zkvm::io::read_vec();
     let mut buf_view = trivial_tree::buf_view::BufView::wrap(&serialized_tree);
-    let db = trivial_tree::Node::parse(&mut buf_view).unwrap();
+    let db = trivial_tree::Node::<32>::parse(&mut buf_view).unwrap();
     let root_hash = db.hash();
 
     println!("done: {:x?}.", root_hash);
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,30 +1,32 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use sp1_sdk::{ProverClient, SP1Stdin};
-use trivial_tree;
+use trivial_tree::Node;
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
-fn main() {
-    let serialized = trivial_tree::random_tree(10, 10);
-
+fn prove_tree(t: &[u8]) {
     // Generate proof.
     let mut stdin = SP1Stdin::new();
-    stdin.write_slice(&serialized);
+    stdin.write_slice(t);
     let client = ProverClient::new();
     let proof = client.prove(ELF, stdin).expect("proving failed");
 
-    // Read output.
-    let found = &proof.public_values.buffer.data;
-    println!("found:        {:x?}", found);
+    // // Read output.
+    // let found = &proof.public_values.buffer.data;
+    // println!("found:        {:x?}", found);
 
     // Verify proof.
-    // client.verify(ELF, &proof).expect("verification failed");
+    client.verify(ELF, &proof).expect("verification failed");
 
-    // Save proof.
-    proof
-        .save("proof-with-io.json")
-        .expect("saving proof failed");
+    // proof
+    //     .save("proof-with-io.json")
+    //     .expect("saving proof failed");
+}
 
-    println!("successfully generated and verified proof for the program!")
+fn main() {
+    let left_tree = Node::<32>::random_tree(5, 10).serialize();
+    let right_tree = Node::<32>::random_tree(5, 10).serialize();
+    prove_tree(&left_tree);
+    prove_tree(&right_tree);
 }
